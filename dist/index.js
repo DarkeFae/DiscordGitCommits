d3a3db916e03b5369c9d35f3b89a43012c402215
DarkeFae <109184801+DarkeFae@users.noreply.github.com>
2024-02-04 18:24:28 +1300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -3,26 +3,6 @@ const get = require('./git.js');
 const fs = require('fs');
 const https = require('https');
 
-const REQUIRED_ENV_VARS = [
-	"WEBHOOK_TOKEN",
-	"WEBHOOK_CHANNEL",
-	"ROLE",
-	"GIT_USERNAME",
-	"GIT_REPOSITORY",
-	"GIT_TOKEN"
-];
-
-process.env.GITHUB_ACTION = process.env.GITHUB_ACTION || '<missing GITHUB_ACTION env var>';
-
-REQUIRED_ENV_VARS.forEach(env => {
-	if (!process.env[env] || !process.env[env].length) {
-		console.error(
-			`Env var ${env} is not defined. Maybe try to set it if you are running the script manually.`
-		);
-		process.exit(1);
-	}
-});
-
 async function main() {
 	try {
 		console.log(`role is ${process.env.ROLE}`)
