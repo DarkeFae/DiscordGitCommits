025d5525d3f6ebcb10c5a4e95a37d692aae30558
DarkeFae <109184801+DarkeFae@users.noreply.github.com>
2024-02-07 19:45:20 +1300

    compat for more embeds, and tidyed up template

---
+++ dist/embeder.js
@@ -0,0 +1,40 @@
+const env = require('dotenv').config();
+const template = require('./template.json');
+const get = require('./git.js');
+
+async function embed() {
+	try {
+		// Call the get function from git.js
+		const response = await get(process.env.GIT_USERNAME, process.env.GIT_REPOSITORY);
+		// regex to split the response into sections
+		let sections = response.split(/\n(?=\[[^\]]+\])/gm);
+		// split the sections into title and content
+		let result = sections.map(section => {
+			let splitIndex = section.indexOf(']') + 1;
+			return [section.substring(0, splitIndex), section.substring(splitIndex)];
+		});
+
+		// create the embeds from the result
+		for (var section in result) {
+			template.embeds.push(
+				{
+					"title": `${result[section][0]}`,
+					"color": process.env.COLOR,
+					"fields": [
+						{
+							"name": "",
+							"value": result[section][1].replaceAll('<done>', '<a:verified:1162750387112124446> '),
+							"inline": false
+						}
+					]
+				}
+			);
+		}
+		return template;
+	}
+	catch (err) {
+		console.error(err)
+	}
+}
+// Export the function
+module.exports = embed;
--- dist/sendToDiscord.js
+++ dist/sendToDiscord.js
@@ -1,67 +1,15 @@
 const dotenv = require('dotenv').config();
-const get = require('./git.js');
+//const get = require('./git.js');
 const fs = require('fs');
 const https = require('https');
+const embeder = require('./embeder.js');
 
 async function sendToDiscord() {
 	try {
 		console.log(`role is ${process.env.ROLE}`)
 		// calls the get function from git.js
-		const response = await get(process.env.GIT_USERNAME, process.env.GIT_REPOSITORY)
-		// splits the response into an array of strings
-		const changesTemp = response.split(/\[.*\]/gm)
-		//removes trailing newlines
-		const changes = changesTemp.slice(1, changesTemp.length)
-		// replaces "<done>" with the checkmark emoji "verified emote"
-		for (i in changes) {
-			changes[i] = changes[i].slice(0, -1)
-			changes[i] = `${changes[i]}`.replace(/<done>/g, '\n<a:verified:1162750387112124446> ')
-
-		}
-		console.log(changes)
-		//im sorry this is in the actual code...
-		const data = JSON.stringify(
-			{
-				"username": "Changelogs",
-				"avatar_url": "",
-				"content": `||<@&${process.env.ROLE}>||`,
-				"embeds": [
-					{
-						"title": "GLOBAL",
-						"color": process.env.COLOR,
-						"fields": [
-							{
-								"name": "",
-								"value": `${changes[0]}`,
-								"inline": false
-							}
-						]
-					},
-					{
-						"title": "DRAGONSPINE SMP",
-						"color": process.env.COLOR,
-						"fields": [
-							{
-								"name": "",
-								"value": `${changes[1]}`,
-								"inline": false
-							}
-						]
-					},
-					{
-						"title": "MISC",
-						"color": process.env.COLOR,
-						"fields": [
-							{
-								"name": "",
-								"value": `${changes[2]}`,
-								"inline": false
-							}
-						]
-					}
-				]
-			});
-
+		const data = JSON.stringify(await embeder());
+		console.log(data)
 		// setup the discord webhook
 		const options = {
 			hostname: 'discord.com',
